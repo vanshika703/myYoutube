6119388077deb0d40549200b5f0bc448bae57cd2
Vanshika <vanshikabhojnagarwala@gmail.com>
2023-04-07 17:18:39 +0530

    fix: add https in search api link

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,7 +5,7 @@ export const YOUTUBE_VIDEOS_URL =
   API_KEY;
 
 export const YOUTUBE_SEARCH_API =
-  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+  "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
 export const sidebarItems = [
   {
